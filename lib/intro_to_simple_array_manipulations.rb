f47a3082bae7023cd19ed2d5bc49fcc2fecf7602
Bailey Wood <woodbmae@gmail.com>
2019-09-17 20:33:42 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -4,21 +4,6 @@ def using_push(array, string)
   updated_array = array.push(string)
 end
 
-describe "using_unshift" do 
-  before(:each) do 
-    bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
-    @new_neighborhood = "Staten Island"
-    @updated_array = using_unshift(bouroughs_in_nyc, @new_neighborhood)
-  end
-  
-  it "takes in two arguments, an array and a string and adds that string to the front of the array using the unshift method" do
-    expect(@updated_array.first).to eq(@new_neighborhood)
-  end
-
-  it "increases the length of the array" do 
-    expect(@updated_array.size).to eq(5)
-  end
-end
 
 def using_unshift(array, string)
   string= "Staten Island"
